04c1e383d4fc8ad303376bb83a724ab04359138c
Hemant Sanvaria <hsanvaria@ccistudios.com>
2017-01-23 15:40:40 -0500

    website launch

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -11,3 +11,14 @@ $aliases['dev'] = array(
 
 	'ssh-options'=>'-p 37241'
 );
+
+$aliases['live'] = array(
+	'uri'=> 'live.mindbridgestrategies.com',
+	'root' => '/home/mindbrid/subdomains/live/public_html',
+	'remote-host'=> 'host.ccistudios.com',
+	'remote-user'=> 'mindbrid',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+	'ssh-options'=>'-p 37241'
+);
